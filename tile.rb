6156a54c367235ee03d96df15f226bb40af2bdff
jacobleesinger <jacobleesinger@gmail.com>
2015-11-01 15:26:33 -0800

    added flags to tile class

--- tile.rb
+++ tile.rb
@@ -1,11 +1,12 @@
 
 class Tile
-  attr_accessor :is_bomb, :revealed, :value
+  attr_accessor :is_bomb, :revealed, :value, :flagged
   def initialize value, board
     @board = board
     @value = value
     @is_bomb = false
     @revealed = false
+    @flagged = false
   end
 
   def set_as_bomb
@@ -16,8 +17,13 @@ class Tile
     @revealed = true
   end
 
+  def toggle_flag
+    @flagged = !@flagged unless revealed
+  end
+
 
   def to_s
+    return "F" if flagged
     return "+" unless revealed
     return "*" if @is_bomb
     return value
